c74d6bf69c8f6e508ed2248c5f2eb0068376fb26
hansuribe <hansuribe@gmail.com>
2024-02-07 21:54:29 -0500

    suma

--- api/app.py
+++ api/app.py
@@ -32,13 +32,15 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         vals = request.form.getlist('misvalores')
-        val1 = float(vals[0])
-        val2 = float(vals[1])
+        # val1 = float(vals[0])
+        # val2 = float(vals[1])
         val3 = ""
         a = diff(val3)
         result = a
     else:
-        val1 = ''
-        val2 = ''
+        # val1 = ''
+        # val2 = ''
+        val3 = ""
         result = ''
-    return render_template('index.html', val1=val1, val2=val2, result=result)
+    # return render_template('index.html', val1=val1, val2=val2, result=result)
+        return render_template('index.html', val3=val3, result=result)
