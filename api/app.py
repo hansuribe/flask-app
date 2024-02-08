212dc60fd3c483c39edac5d0153ce56392bb073e
hansuribe <hansuribe@gmail.com>
2024-02-07 21:38:31 -0500

    suma

--- api/app.py
+++ api/app.py
@@ -15,13 +15,14 @@ from flask import Flask, request, render_template
 from sympy import *
 
 
-val1 = Symbol('x')
+x = Symbol('x')
 
 
 
 
 
 #Se calcula la derivada de la funcion cos(sin(x^3)) con respecto a x
+
 
 
 app = Flask(__name__)
@@ -31,13 +32,10 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         vals = request.form.getlist('misvalores')
-        # val1 = float(vals[0])
-        # val2 = float(vals[1])
-        val1 = Symbol('x')
-        # val2 = Symbol('x')
-        # val2 = float(vals[1])
-
-        result = diff(val1)
+        val1 = float(vals[0])
+        val2 = float(vals[1])
+        a = diff(cos(sin(x**3)),x)
+        result = a
     else:
         val1 = ''
         val2 = ''
