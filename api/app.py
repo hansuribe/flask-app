c3fb8e1ff503cd68d1810551e8a9741ec80b54b8
hansuribe <hansuribe@gmail.com>
2024-02-07 22:58:18 -0500

    suma

--- api/app.py
+++ api/app.py
@@ -1,55 +1,3 @@
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def home():
-#     return 'Aplicacion para potenciar ,Multiplicaciones '
-#
-# @app.route('/about')
-# def about():
-#     return 'About'
-
-
-from flask import Flask, request, render_template
-from sympy import symbols, diff, simplify
-
-
-x = Symbol('x')
-
-
-
-
-
-#Se calcula la derivada de la funcion cos(sin(x^3)) con respecto a x
-
-
-
-app = Flask(__name__)
-
-#
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if request.method == 'POST':
-#
-#
-#         vals = request.form.getlist('misvalores')
-#         val3 = float(vals['x'])
-#         # val2 = float(vals[1])
-#         # val3 = Symbol(vals[0])
-#
-#         a =  diff(val3)
-#
-#
-#         result = a
-#     else:
-#         # val1 = ''
-#         # val2 = ''
-#         val3 = ""
-#         result = ''
-#     # return render_template('index.html', val1=val1, val2=val2, result=result)
-#         return render_template('index.html',  result=result)
-
 from flask import Flask, render_template, request
 from sympy import symbols, diff, simplify
 
@@ -57,6 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    resultado = None
     if request.method == 'POST':
         # Obtener la función ingresada por el usuario desde el formulario
         funcion = request.form['funcion']
@@ -73,9 +22,4 @@ def index():
         except Exception as e:
             resultado = f"No se pudo derivar la función: {e}"
 
-        return render_template('index.html', resultado=resultado)
-    else:
-        return render_template('index.html', resultado=None)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template('index.html', resultado=resultado)
