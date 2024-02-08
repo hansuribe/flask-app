9df814a01b020b76355580221c6d49dadfe9952c
hansuribe <hansuribe@gmail.com>
2024-02-07 22:05:07 -0500

    suma

--- api/app.py
+++ api/app.py
@@ -34,7 +34,7 @@ def index():
         vals = request.form.getlist('misvalores')
         # val1 = float(vals[0])
         # val2 = float(vals[1])
-        val3 = Symbol(vals[])
+        val3 = Symbol(vals[0])
         a = diff(val3)
         result = a
     else:
