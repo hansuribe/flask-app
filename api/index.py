94997d1e06aa8a4c25d852a5300c77ca7eae2629
hansuribe <hansuribe@gmail.com>
2024-02-07 19:37:36 -0500

    agregando programa suma

--- api/index.py
+++ api/index.py
@@ -1,11 +1,29 @@
-from flask import Flask
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def home():
+#     return 'Aplicacion para potenciar ,Multiplicaciones '
+#
+# @app.route('/about')
+# def about():
+#     return 'About'
+
+
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Aplicacion para potenciar ,Multiplicaciones '
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        vals = request.form.getlist('misvalores')
+        val1 = float(vals[0])
+        val2 = float(vals[1])
+        result = val1 + val2
+    else:
+        val1 = ''
+        val2 = ''
+        result = ''
+    return render_template('index.html', val1=val1, val2=val2, result=result)
