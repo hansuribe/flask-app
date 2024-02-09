f986c6d3e7271a1dd98580dee93685b3d58a13b3
hansuribe <hansuribe@gmail.com>
2024-02-08 21:13:07 -0500

    first commit

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from sympy import symbols, diff, integrate, simplify, SympifyError
 
 import random
